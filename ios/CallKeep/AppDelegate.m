996d2129aa1baf8a401d54bb7c8ddc0f6c24d2e1
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-01-28 16:41:19 -0300

    implements react-native-voip-push-notification

--- ios/CallKeep/AppDelegate.m
+++ ios/CallKeep/AppDelegate.m
@@ -5,6 +5,9 @@
 #import <React/RCTRootView.h>
 #import "RNCallKeep.h"
 
+#import <PushKit/PushKit.h>                    /* <------ add this line */
+#import "RNVoipPushNotificationManager.h"      /* <------ add this line */
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -33,9 +36,18 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  
+  // ===== (THIS IS OPTIONAL BUT RECOMMENDED) =====
+   // --- register VoipPushNotification here ASAP rather than in JS. Doing this from the JS side may be too slow for some use cases
+   // --- see: https://github.com/react-native-webrtc/react-native-voip-push-notification/issues/59#issuecomment-691685841
+   [RNVoipPushNotificationManager voipRegistration];
+   // ===== (THIS IS OPTIONAL BUT RECOMMENDED) =====
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"CallKeep"
                                             initialProperties:nil];
+  
+  
 
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
@@ -67,6 +79,44 @@ continueUserActivity:(NSUserActivity *)userActivity
   return [RNCallKeep application:application
            continueUserActivity:userActivity
              restorationHandler:restorationHandler];
+}
+
+/* Add PushKit delegate method */
+
+// --- Handle updated push credentials
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(PKPushType)type {
+  // Register VoIP push token (a property of PKPushCredentials) with server
+  [RNVoipPushNotificationManager didUpdatePushCredentials:credentials forType:(NSString *)type];
+}
+
+- (void)pushRegistry:(PKPushRegistry *)registry didInvalidatePushTokenForType:(PKPushType)type
+{
+  // --- The system calls this method when a previously provided push token is no longer valid for use. No action is necessary on your part to reregister the push type. Instead, use this method to notify your server not to send push notifications using the matching push token.
+}
+
+// --- Handle incoming pushes
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)(void))completion {
+  
+
+  // --- NOTE: apple forced us to invoke callkit ASAP when we receive voip push
+  // --- see: react-native-callkeep
+
+  // --- Retrieve information from your voip push payload
+  NSString *uuid = payload.dictionaryPayload[@"uuid"];
+  NSString *callerName = [NSString stringWithFormat:@"%@ (Connecting...)", payload.dictionaryPayload[@"callerName"]];
+  NSString *handle = payload.dictionaryPayload[@"handle"];
+
+  // --- this is optional, only required if you want to call `completion()` on the js side
+  [RNVoipPushNotificationManager addCompletionHandler:uuid completionHandler:completion];
+
+  // --- Process the received push
+  [RNVoipPushNotificationManager didReceiveIncomingPushWithPayload:payload forType:(NSString *)type];
+
+  // --- You should make sure to report to callkit BEFORE execute `completion()`
+  [RNCallKeep reportNewIncomingCall:uuid handle:handle handleType:@"generic" hasVideo:false localizedCallerName:callerName fromPushKit: YES payload:nil];
+  
+  // --- You don't need to call it if you stored `completion()` and will call it on the js side.
+  completion();
 }
 
 @end
--- src/screen/Contact.js
+++ src/screen/Contact.js
@@ -1,10 +1,13 @@
 import React, {useLayoutEffect, useState, useEffect, useContext} from 'react';
 import {Platform, Text, TouchableOpacity, View} from 'react-native';
+import VoipPushNotification from 'react-native-voip-push-notification';
 import RNCallKeep from 'react-native-callkeep';
 import BackgroundTimer from 'react-native-background-timer';
 import {UserList} from '../component';
 import styles from '../style/Contact.style';
 import {CallContext} from '../context';
+import {checkNotifications} from 'react-native-permissions';
+import {requestNotifications} from 'react-native-permissions';
 
 BackgroundTimer.start();
 
@@ -62,12 +65,66 @@ const Contact = ({navigation}) => {
     navigation.navigate('Call', {callUUID});
   };
 
+  const handleVoip = () => {
+    // or anywhere which is most comfortable and appropriate for you
+    //VoipPushNotification.requestPermissions(); // --- optional, you can use another library to request permissions
+    VoipPushNotification.registerVoipToken(); // --- required
+
+    VoipPushNotification.addEventListener('register', token => {
+      console.log('VOIP Token: ', token);
+      // --- send token to your apn provider server
+    });
+
+    VoipPushNotification.addEventListener('localNotification', notification => {
+      console.warn('Local notification clicked ', notification);
+      // --- when user click local push
+    });
+
+    VoipPushNotification.addEventListener('notification', notification => {
+      console.warn('Notification received: ', notification);
+      // --- when receive remote voip push, register your VoIP client, show local notification ... etc
+      //this.doRegisterOrSomething();
+
+      // --- This  is a boolean constant exported by this module
+      // --- you can use this constant to distinguish the app is launched by VoIP push notification or not
+      if (VoipPushNotification.wakeupByPush) {
+        console.warn('waked up by VOIP');
+        // this.doSomething()
+
+        // --- remember to set this static variable back to false
+        // --- since the constant are exported only at initialization time, and it will keep the same in the whole app
+        VoipPushNotification.wakeupByPush = false;
+      }
+
+      /**
+       * Local Notification Payload
+       *
+       * - `alertBody` : The message displayed in the notification alert.
+       * - `alertAction` : The "action" displayed beneath an actionable notification. Defaults to "view";
+       * - `soundName` : The sound played when the notification is fired (optional).
+       * - `category`  : The category of this notification, required for actionable notifications (optional).
+       * - `userInfo`  : An optional object containing additional notification data.
+       */
+      console.warn('Presenting local notification ');
+
+      VoipPushNotification.presentLocalNotification({
+        alertBody: 'hello! ' + notification.getMessage(),
+      });
+    });
+  };
+
   useEffect(() => {
     RNCallKeep.addEventListener('answerCall', answerCall);
+    requestNotifications(['alert', 'sound']).then(({status, settings}) => {});
+
+    checkNotifications().then(({status, settings}) => {});
+    handleVoip();
+
     return () => {
       RNCallKeep.removeEventListener('answerCall', answerCall);
     };
   }, []);
+
   return (
     <View style={styles.container}>
       <UserList displayIncomingCall={displayIncomingCall} />
